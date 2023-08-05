e80125417c6c9f10aadadac7c37ff84f53a44cda
liponex <me+supermodbus@liponex.ru>
2023-08-05 13:41:09 +0500

    Dependencies update + removed don't needed code + some work on serial configuration

---
+++ serial/serial_configs.go
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2023 liponex
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the terms of the GNU General Public License as published by
+ * the  Free Software Foundation, either version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.licenses/>.
+ */
+
+package serial
+
+var (
+	DefaultRates = []string{
+		"110",
+		"300",
+		"600",
+		"1200",
+		"2400",
+		"4800",
+		"9600",
+		"14400",
+		"19200",
+		"38400",
+		"56000",
+		"57600",
+		"115200",
+		"128000",
+		"256000",
+		"230400",
+		"460800",
+		"921600",
+	}
+)
